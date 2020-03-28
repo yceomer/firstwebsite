5ec81c66a0d0915f7564f529ee6afe71791b5270
yceomer <yuce.omer@outlook.com>
2020-03-28 19:43:21 +0300

    ömer güncelleme

--- js/main.js
+++ js/main.js
@@ -11,6 +11,17 @@ jQuery(document).ready(function( $ ) {
 
   });
 
+    if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+		var typed = new Typed('.text-slider', {
+			strings: typed_strings.split(','),
+			typeSpeed: 80,
+			loop: true,
+			backDelay: 1100,
+			backSpeed: 30
+		});
+	}
+
   // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
